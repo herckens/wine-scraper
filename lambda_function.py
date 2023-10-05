2ef29e35e0e36e98b014a52f8856b03c4a638edf
Hendrik Erckens <Hendrik.Erckens@swisscom.com>
2023-10-05 15:02:43 +0200

    Extract input

--- lambda_function.py
+++ lambda_function.py
@@ -1,17 +1,22 @@
+import json
 import requests
 from bs4 import BeautifulSoup
 
 def lambda_handler(event, context):
-    # url = event['url']
-    # # URL = "https://www.bindella.ch/weinshop/villa-antinori-riserva-chianti-classico-docg-riserva-antinori-nel-chianti-classico1917"
-    # page = requests.get(url)
-    # soup = BeautifulSoup(page.content, "html.parser")
-    # degustation_element = soup.find("div", {"class": "product attribute degustation"}).find("div", {"class": "value"})
+    body = event['body']
+    content = json.loads(body)
 
-    # degu_notes = degustation_element.text.strip()
+    url = content['url']
+    # URL = "https://www.bindella.ch/weinshop/villa-antinori-riserva-chianti-classico-docg-riserva-antinori-nel-chianti-classico1917"
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    degustation_element = soup.find("div", {"class": "product attribute degustation"}).find("div", {"class": "value"})
+
+    degu_notes = degustation_element.text.strip()
 
     return {
         'statusCode': 200,
-        'body': event
-        # 'body': degu_notes
+        # 'body': json.dumps(event['body'])
+        # 'body': content['url']
+        'body': degu_notes
     }
