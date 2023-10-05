9e67a7735d60d1437fb4c734a2d99ebcf68004c9
Hendrik Erckens <Hendrik.Erckens@swisscom.com>
2023-10-05 22:00:42 +0200

    Add vinification

--- lambda_function.py
+++ lambda_function.py
@@ -21,8 +21,10 @@ def get_wine_details(url: str) -> dict:
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
     degu_notes = extract_degu_notes(soup)
+    vinification = extract_vinification(soup)
     return {
-        "degu_notes": degu_notes
+        "degu_notes": degu_notes,
+        "vinification": vinification
     }
 
 
@@ -31,6 +33,13 @@ def extract_degu_notes(soup):
         "div", {"class": "product attribute degustation"}).find("div", {"class": "value"})
     degu_notes = degustation_element.text.strip()
     return degu_notes
+
+
+def extract_vinification(soup):
+    vinification_element = soup.find(
+        "div", {"class": "product attribute vinification"}).find("div", {"class": "value"})
+    vinification = vinification_element.text.strip()
+    return vinification
 
 
 def main(args):
