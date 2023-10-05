de0a5a4dfc602a84af0e0702f96c065af6c38bee
Hendrik Erckens <Hendrik.Erckens@swisscom.com>
2023-10-05 14:47:44 +0200

    Return event

--- lambda_function.py
+++ lambda_function.py
@@ -2,15 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 
 def lambda_handler(event, context):
-    url = event['url']
-    # URL = "https://www.bindella.ch/weinshop/villa-antinori-riserva-chianti-classico-docg-riserva-antinori-nel-chianti-classico1917"
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, "html.parser")
-    degustation_element = soup.find("div", {"class": "product attribute degustation"}).find("div", {"class": "value"})
+    # url = event['url']
+    # # URL = "https://www.bindella.ch/weinshop/villa-antinori-riserva-chianti-classico-docg-riserva-antinori-nel-chianti-classico1917"
+    # page = requests.get(url)
+    # soup = BeautifulSoup(page.content, "html.parser")
+    # degustation_element = soup.find("div", {"class": "product attribute degustation"}).find("div", {"class": "value"})
 
-    degu_notes = degustation_element.text.strip()
+    # degu_notes = degustation_element.text.strip()
 
     return {
         'statusCode': 200,
-        'body': degu_notes
+        'body': event
+        # 'body': degu_notes
     }
