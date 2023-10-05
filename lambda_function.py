43156dbbf4a779ba5d56355228e216d29f18f41e
Hendrik Erckens <Hendrik.Erckens@swisscom.com>
2023-10-05 20:57:02 +0200

    Enable local debugging in vscode

--- lambda_function.py
+++ lambda_function.py
@@ -1,22 +1,36 @@
+import sys
 import json
 import requests
 from bs4 import BeautifulSoup
 
+
 def lambda_handler(event, context):
     body = event['body']
-    content = json.loads(body)
+    request_content = json.loads(body)
+    url = request_content['url']
 
-    url = content['url']
-    # URL = "https://www.bindella.ch/weinshop/villa-antinori-riserva-chianti-classico-docg-riserva-antinori-nel-chianti-classico1917"
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, "html.parser")
-    degustation_element = soup.find("div", {"class": "product attribute degustation"}).find("div", {"class": "value"})
-
-    degu_notes = degustation_element.text.strip()
+    wine_details = get_wine_details(url)
 
     return {
         'statusCode': 200,
-        # 'body': json.dumps(event['body'])
-        # 'body': content['url']
-        'body': degu_notes
+        'body': wine_details
     }
+
+
+def get_wine_details(url: str) -> dict:
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    degustation_element = soup.find(
+        "div", {"class": "product attribute degustation"}).find("div", {"class": "value"})
+
+    degu_notes = degustation_element.text.strip()
+    return degu_notes
+
+
+def main(args):
+    url = args[0]
+    print(get_wine_details(url))
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
