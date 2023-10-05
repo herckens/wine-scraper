9a4ca08e2f448fa376d7b5d991736466b9017dd4
Hendrik Erckens <Hendrik.Erckens@swisscom.com>
2023-10-05 13:50:38 +0200

    Add AWS Lambda

---
+++ lambda_function.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+def lambda_handler(event, context):
+    URL = "https://www.bindella.ch/weinshop/villa-antinori-riserva-chianti-classico-docg-riserva-antinori-nel-chianti-classico1917"
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    degustation_element = soup.find("div", {"class": "product attribute degustation"}).find("div", {"class": "value"})
+
+    degu_notes = degustation_element.text.strip()
+
+    return {
+        'statusCode': 200,
+        'body': degu_notes
+    }
